e26b26e465d1716274aa7157f4e7d6f24cfaf11b
Andreyy320 <andreymartinenko90@mail.ru>
2026-08-22 20:31:39 +0300

    ПОНИМАНИЕ ЧТО ЭТО ПОЛЬЗОВАТЕЛЬ

--- routes/api.js
+++ routes/api.js
@@ -2090,6 +2090,21 @@ router.put('/moves/:id/post', async (req, res) => {
 
 
 
+// Запись лога (POST)
+    router.post('/logs', async (req, res) => {
+        const { userId, action, details } = req.body;
+        try {
+            await pool.query(
+                `INSERT INTO audit_logs (user_id, action, details) VALUES ($1, $2, $3)`,
+                [userId || null, action, details || '']
+            );
+            res.json({ success: true });
+        } catch (err) {
+            console.error('Ошибка записи лога:', err.message);
+            res.status(500).send('Ошибка сервера');
+        }
+    });
+
 
 
 // ==================== УНИВЕРСАЛЬНЫЙ POST С ЛОГИРОВАНИЕМ ====================
@@ -2443,6 +2458,7 @@ router.post('/:entity', async (req, res) => {
         res.status(500).json({ error: 'Ошибка сервера при добавлении: ' + err.message });
     }
 });
+
 // ==================== УНИВЕРСАЛЬНЫЙ PUT (ПРОФЕССИОНАЛЬНЫЙ С ПРОВЕРКОЙ ОСТАТКОВ И ЗАЩИТОЙ ПРОВЕДЕННЫХ ДОКУМЕНТОВ) ====================
 router.put('/:entity/:id', async (req, res) => {
     const client = await pool.connect();
@@ -2804,23 +2820,6 @@ router.delete('/:entity/:id', async (req, res) => {
 });
 
 
-
-
-
-// Запись лога (POST)
-    router.post('/logs', async (req, res) => {
-        const { userId, action, details } = req.body;
-        try {
-            await pool.query(
-                `INSERT INTO audit_logs (user_id, action, details) VALUES ($1, $2, $3)`,
-                [userId || null, action, details || '']
-            );
-            res.json({ success: true });
-        } catch (err) {
-            console.error('Ошибка записи лога:', err.message);
-            res.status(500).send('Ошибка сервера');
-        }
-    });
 
 
 
