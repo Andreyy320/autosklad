8ab44b3921a6be25a75290482700cb59e940b1a5
Andreyy320 <andreymartinenko90@mail.ru>
2026-09-08 20:44:50 +0300

    Initial commit

--- public/script.js
+++ public/script.js
@@ -2783,7 +2783,6 @@ async function openReceiptForm(entity, item = null) {
 
     const config = getConfig('receipts');
     const drawer = getOrCreateDrawer();
-    
     const now = new Date();
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0');
