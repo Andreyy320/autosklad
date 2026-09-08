2c199ac7666f633a8fce35664af323377da333c9
Andreyy320 <andreymartinenko90@mail.ru>
2026-09-08 20:37:18 +0300

    Initial commit

--- public/script.js
+++ public/script.js
@@ -3188,6 +3188,7 @@ async function openReceiptForm(entity, item = null) {
         }
     });
 }
+
 async function openMoveForm(entityOrItem, itemArg = null, parentIdArg = null) {
     // УМНАЯ НОРМАЛИЗАЦИЯ АРГУМЕНТОВ (защита от перепутанных параметров при вызове из разных мест)
     let entity, item, parentId;
