cd5ef1c7f4f25c2eb87719df4e75a4268a780156
Andrey <andrey@example.com>
2026-09-09 11:22:19 +0300

    ff

--- public/script.js
+++ public/script.js
@@ -798,6 +798,7 @@ const tableConfig = {
         { field: 'code', label: 'Код', width: '90px', align: 'center' },
         { field: 'name', label: 'Наименование', width: '280px' },
         { field: 'manufacturer', label: 'Производитель', width: '130px' },
+        { field: 'current_sklad', label: 'Текущий склад', width: '140px' },
         { field: 'unit', label: 'Ед.изм', width: '60px', align: 'center' },
         { field: 'income_qty', label: 'Кол-во', width: '60px', align: 'right' },
         { field: 'income_sum', label: 'Сумма', width: '80px', align: 'right' },
@@ -818,6 +819,7 @@ const tableConfig = {
             <td style="text-align: center;">${item.code || ''}</td>
             <td><b>${item.name || ''}</b></td>
             <td>${item.manufacturer || ''}</td>
+            <td><b>${item.current_sklad || '—'}</b></td>
             <td style="text-align: center;">${item.unit || 'шт'}</td>
             
             <!-- Приход -->
@@ -836,7 +838,6 @@ const tableConfig = {
         `;
     }
     },
-
     accidents: {
     title: 'ДТП',
     columns: [
