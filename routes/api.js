34c3c1073e51a1a1ea424f703b439133f3ffee0d
Andreyy320 <andreymartinenko90@mail.ru>
2026-09-15 22:57:16 +0300

    FFF

--- routes/api.js
+++ routes/api.js
@@ -5674,7 +5674,7 @@ router.get('/expenses_by_suppliers/:id/payments', async (req, res) => {
     FROM supplier_payments sp
     LEFT JOIN receipts rec ON sp.receipt_id = rec.id
     WHERE sp.supplier_id = $1
-      AND ($2::text IS NULL OR TO_CHAR(rec.date, 'YYYY-MM') = $2)
+      AND ($2::text IS NULL OR TO_CHAR(sp.date, 'YYYY-MM') = $2)
     ORDER BY sp.date DESC, sp.id DESC;
     `;
 
